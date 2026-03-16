030df7a9873589b26d083baf5bf638304bfb9344
Snaily <snailycfx@gmail.com>
2026-03-16 10:11:16 +0100

    feat: add roblox-ts TypeScript hook to avoid ts-patch requirement

--- src/index.ts
+++ src/index.ts
@@ -1,145 +1,61 @@
-import ts from "typescript";
+// https://github.com/rbxts-flamework/transformer/blob/master/src/index.ts
 
-const EVENT_HANDLER_DECORATOR = 'EventHandler';
-const NEXUS_METADATA_CALL = 'NexusMetadata';
+import { existsSync } from "fs";
+import Module from "module";
+import path from "path";
 
-export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
-    const checker = program.getTypeChecker();
+const cwd = process.cwd();
+const originalRequire = Module.prototype.require as NodeJS.Require;
 
-    return (context: ts.TransformationContext): ts.Transformer<ts.SourceFile> => {
-        return (sourceFile: ts.SourceFile): ts.SourceFile => {
-            const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
-                if (ts.isClassDeclaration(node)) {
-                    return visitClassDeclaration(checker, context, node);
-                }
+function shouldHook(): boolean {
+    if (!existsSync(path.join(cwd, "tsconfig.json"))) return false;
+    if (!existsSync(path.join(cwd, "package.json"))) return false;
+    if (!existsSync(path.join(cwd, "node_modules"))) return false;
 
-                if (
-                    ts.isSourceFile(node) ||
-                    ts.isModuleDeclaration(node) ||
-                    ts.isModuleBlock(node)
-                ) {
-                    return ts.visitEachChild(node, visitor, context);
-                }
-
-                return node;
-            }
-
-            return ts.visitNode(sourceFile, visitor) as ts.SourceFile;
-        }
-    }
+    return true;
 }
 
-function visitSourceFile(checker: ts.TypeChecker, context: ts.TransformationContext): ts.Visitor {
-    return (node: ts.Node): ts.VisitResult<ts.Node> => {
-        if (ts.isClassDeclaration(node)) {
-            return visitClassDeclaration(checker, context, node);
-        }
-
-        return ts.visitEachChild(node, visitSourceFile(checker, context), context);
-    }
-}
-
-function visitClassDeclaration(checker: ts.TypeChecker, context: ts.TransformationContext, classNode: ts.ClassDeclaration): ts.ClassDeclaration {
-    const updatedMembers = classNode.members.map((member) => {
-        if (!ts.isMethodDeclaration(member)) return member;
-        if (!hasEventHandlerDecorator(member)) return member;
-
-        return injectMetadata(checker, context, member);
-    })
-
-    return ts.factory.updateClassDeclaration(
-        classNode,
-        classNode.modifiers,
-        classNode.name,
-        classNode.typeParameters,
-        classNode.heritageClauses,
-        updatedMembers
-    );
-}
-
-function injectMetadata(checker: ts.TypeChecker, context: ts.TransformationContext, method: ts.MethodDeclaration): ts.MethodDeclaration {
-    const firstParam = method.parameters[0];
-    if (firstParam === undefined) return method;
-
-    const eventClassName = resolveParamTypeName(checker, firstParam);
-    if (eventClassName === undefined) return method;
-
-    // Generates: NexusMetadata.define('paramtypes', [EventClassName], target, 'methodName')
-    const metadataCall = buildMetadataCall(method, eventClassName);
-    const originalBody = method.body;
-    
-    if (originalBody === undefined) {
-        return method;
-    }
-
-    const updatedBody = ts.factory.updateBlock(originalBody, [
-        ts.factory.createExpressionStatement(metadataCall),
-        ...originalBody.statements
-    ]);
-
-    return ts.factory.updateMethodDeclaration(
-        method,
-        method.modifiers,
-        method.asteriskToken,
-        method.name,
-        method.questionToken,
-        method.typeParameters,
-        method.parameters,
-        method.type,
-        updatedBody,
-    );
-}
-
-function buildMetadataCall(method: ts.MethodDeclaration, eventClassName: string): ts.CallExpression {
-    const methodName = (method.name as ts.Identifier).text;
-    
-    return ts.factory.createCallExpression(
-        ts.factory.createPropertyAccessExpression(
-            ts.factory.createIdentifier(NEXUS_METADATA_CALL),
-            ts.factory.createIdentifier('define')
-        ),
-        undefined,
-        [
-            ts.factory.createStringLiteral("paramtypes"),
-            ts.factory.createArrayLiteralExpression([
-                ts.factory.createIdentifier(eventClassName),
-            ]),
-            ts.factory.createIdentifier("target"),
-            ts.factory.createStringLiteral(methodName),
-        ]
-    )
-}
-
-function hasEventHandlerDecorator(method: ts.MethodDeclaration): boolean {
-    return (method.modifiers ?? []).some((modifier) => {
-        if (!ts.isDecorator(modifier)) return false;
-
-        const expression = modifier.expression;
-
-        // @EventHandler() - CallExpression
-        if (ts.isCallExpression(expression)) {
-            const callee = expression.expression;
-            return ts.isIdentifier(callee) && callee.text === EVENT_HANDLER_DECORATOR;
-        }
-
-        if (ts.isIdentifier(expression)) {
-            return expression.text === EVENT_HANDLER_DECORATOR;
-        }
-
-        return false;
-    })
-}
-
-function resolveParamTypeName(checker: ts.TypeChecker, param: ts.ParameterDeclaration): string | undefined {
-    const typeNode = param.type;
-    if (typeNode === undefined) return undefined;
-
-    const type = checker.getTypeFromTypeNode(typeNode);
-    const symbol = type.getSymbol();
-
-    if (symbol === undefined) {
+function tryResolve(id: string, basePath: string): string | undefined {
+    try {
+        return require.resolve(id, { paths: [basePath] });
+    } catch {
         return undefined;
-    };
-
-    return symbol.getName();
+    }
 }
+
+function isDescendantOf(filePath: string, parentPath: string): boolean {
+    const relative = path.relative(parentPath, filePath);
+    return !relative.startsWith("..") && !path.isAbsolute(relative);
+}
+
+function hook(): void {
+    const robloxTsPath = tryResolve("roblox-ts", cwd);
+    if (!robloxTsPath) return;
+
+    const robloxTsTypeScriptPath = tryResolve("typescript", robloxTsPath);
+    if (!robloxTsTypeScriptPath) return;
+
+    const nexusTypeScript = require("typescript");
+    const robloxTsTypeScript = require(robloxTsTypeScriptPath);
+    if (nexusTypeScript === robloxTsTypeScript) return;
+
+    ;(Module.prototype as NodeJS.Module).require = function nexusHook(
+        this: NodeJS.Module,
+        id: string,
+    ) {
+        if (id === "typescript" && isDescendantOf(this.filename, __dirname)) {
+            return robloxTsTypeScript;
+        }
+
+        return originalRequire.call(this, id);
+    } as NodeJS.Require
+}
+
+if (shouldHook()) {
+    hook()
+}
+
+const transformer = require('./transformer');
+Module.prototype.require = originalRequire;
+
+export = transformer;
